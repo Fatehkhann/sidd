47ebf2b79f1ed820a1553e07c82b68d988541517
fateh khan <fateh khan>
2017-03-25 23:58:49 +0500

    Second Commit (Port Number Correction)

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const publicPath = path.join(__dirname + './../public');
-
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
     res.render('index.hbs');
 });
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up and tunning on port 3000');
 })
 
